ccf0fad299112d25284810acd91251964263407c
Gerson1120 <20223tn144@utez.edu.mx>
2025-09-26 10:59:32 -0600

    update rutas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
     .then(registration => {
       console.log('✅ Service Worker registrado.');
     }).catch(error => {
